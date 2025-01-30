1e051c760ad3b5dc161594455adfcdee4632c443
yogendra-silencelabs <yogendra@silencelaboratories.com>
2025-01-30 21:38:31 +0530

    context improvement

--- packages/plugin-sl/src/index.ts
+++ packages/plugin-sl/src/index.ts
@@ -1,14 +1,15 @@
 export * from "./providers/wallet";
 export * from "./types";
+export * from "./actions/keygen"
+export * from  "./actions/sign"
 
 import type { Plugin } from "@elizaos/core";
-import { signAction } from "./actions/sign"
 import { keygenAction } from "./actions/keygen";
-
+import { signAction } from "./actions/sign";
+import {WalletProvider} from "./providers/wallet"
 export const slPlugin: Plugin = {
     name: "sl",
     description: "Silence Laboratories: Silent Network plugin for Eliza",
-    providers: [],
     evaluators: [],
     services: [],
     actions: [signAction, keygenAction],
